f4c5f03d9798b2e9b4204ab79648a0f6f3858c07
Carly Stasack <carlystasack@gmail.com>
2018-05-30 11:53:43 -0700

    can add to WebUsers table

--- createAccount.php
+++ createAccount.php
@@ -14,37 +14,38 @@
 
 <?php
   include "header.php";
-  $msg = "Login";
+  $msg = "Create Account";
   include 'connectvars.php';
   $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
   if (!$conn) {
     die('Could not connect: ' . mysql_error());
   }
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Escape user inputs for security
     	$username = mysqli_real_escape_string($conn, $_POST['username']);
       $password = mysqli_real_escape_string($conn, $_POST['password']);
-      $hashedpassword = MD5('$password');
-    //  echo $hashedpassword;
-      //$queryIn = mysql_query("SELECT * FROM Users where username='$username' AND password='MD5($password)'");
-      //$resultIn = msqli_query($conn, $queryIn);
+      $name = mysqli_real_escape_string($conn, $_POST['name']);
+      $phoneNumber = mysqli_real_escape_string($conn, $_POST['phoneNumber']);
 
-        $queryIn = "SELECT * FROM Users where username='$username' AND password=MD5('$password')";
-    		$resultIn = mysqli_query($conn, $queryIn);
-    		if (mysqli_num_rows($resultIn)> 0) {
-    			$msg ="<h2>You have successfully logged in!</h2>";
-    		}
-        else{
-          $msg = "<h2>The username or password you entered are incorrect.</h2>";
-        }/*
-      if(mysql_fetch_rows()>0) {
-      echo "You have succesfully logged in!";
-    }
-    else{
-      echo "The username or password you entered are incorrect.";
-    }*/
+      // See if user is already in the table
+      		$queryIn = "SELECT * FROM WebUsers where username='$username' ";
+      		$resultIn = mysqli_query($conn, $queryIn);
+      		if (mysqli_num_rows($resultIn)> 0) {
+      			$msg ="<h2>Error: Please choose another username. That one already exists.</h2>";
+      		} else {
 
-    }
+      		// attempt insert query
 
+      			$query = "INSERT INTO WebUsers (username, password, name, phoneNumber) VALUES ('$username', '$password', '$name', '$phoneNumber')";
+      			if(mysqli_query($conn, $query)){
+      				$msg =  "<p>Account successfully created.</p>";
+      			} else{
+      				echo "ERROR: Could not execute $query. " . mysqli_error($conn);
+      			}
+      		}
+      }
+
+// close connection
   mysqli_close($conn);
 
 ?>
@@ -62,6 +63,14 @@
   <p>
       <label for="password">Password:</label>
       <input type="text" class="required" name="password" id="password">
+  </p>
+  <p>
+      <label for="name">Name:</label>
+      <input type="text" class="required" name="name" id="name">
+  </p>
+  <p>
+      <label for="phoneNumber">Phone Number:</label>
+      <input type="number" min=1111111111 max = 9999999999 class="required" name="phoneNumber" id="phoneNumber" title="phone number should be numeric">
   </p>
 </fieldset>
 
