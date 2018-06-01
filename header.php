de7e904811640f01ee37dbc6f4534539602f939b
Melissa Kiersey <kierseym@flip1.engr.oregonstate.edu>
2018-05-31 19:44:57 -0700

    added stuff to home

--- header.php
+++ header.php
@@ -1,15 +1,30 @@
-
-
+<?php
+	session_start();
+	if(isset($_SESSION["name"])){
+		$user = $_SESSION["name"];
+	}
+	else{
+		$user = " ";
+	}
+ ?>
 	<header>
-		Restaurant Reviews - <em><span id="username"><?php echo $user;?></span>Welcome!</em>
+		Restaurant Reviews<em><span id="username"><?php echo $user?></span></em>
 	</header>
 	<nav>
 		<ul>
 		<?php
 		foreach ($content as $page => $location){
-			echo "<li><a href='$location?user=".$user."' ".($page==$currentpage?" class='active'":"").">".$page."</a></li>";
+			echo "<li><a href='$location?user=".$username."' ".($page==$currentpage?" class='active'":"").">".$page."</a></li>";
 		}
 		?>
+		<li>
+			<?php
+				session_start();
+					session_destroy();
+
+				?>
+				<a href="home.php">Logout</a></li>
+	</ul>
 <!--
 		<li><a href="/signUp.php">Sign up</a></li>
 		<li><a href="/listUsers.php">List Users</a><li>
