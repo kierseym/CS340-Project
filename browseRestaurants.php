67b5a6233880b49290a58df8737120f5b72675aa
Carly Stasack <carlystasack@gmail.com>
2018-06-05 11:31:10 -0700

    search sort of working

--- browseRestaurants.php
+++ browseRestaurants.php
@@ -21,50 +21,90 @@
   if (!$conn) {
     die('Could not connect: ' . mysql_error());
   }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  // query to select all information from supplier table
-  	$query = "SELECT * FROM Restaurants ";
-    // Get results from query
-    	$result = mysqli_query($conn, $query);
-    	if (!$result) {
-    		die("Query to show fields from table failed");
-    	}
-    // get number of columns in table
-    //	$fields_num = mysqli_num_fields($result);
-    	echo "<h1>Restaurants:</h1>";
-    	//echo "<table id='t01' border='1'><tr>";
+    // Escape user inputs for security
+      $name = mysqli_real_escape_string($conn, $_POST['name']);
+      $rating = mysqli_real_escape_string($conn, $_POST['rating']);
+      $type = mysqli_real_escape_string($conn, $_POST['type']);
+      $city = mysqli_real_escape_string($conn, $_POST['city']);
 
-    	while($row = mysqli_fetch_row($result)) {
-    echo"<div class='restaurant'>
-        <div class='restaurant-contents'>
-          <div class='restaurant-image-container'>
-            <img src=$row[6]>
+    // query to select all information from supplier table
+    	$query = "SELECT * FROM Restaurants
+                WHERE name = '$name' and rating = '$rating'
+                      and type = '$type' and city = '$city'";
+      // Get results from query
+      	$result = mysqli_query($conn, $query);
+      	if (!$result) {
+      				echo "ERROR: Could not execute $query. " . mysqli_error($conn);
+      	}
+      // get number of columns in table
+      //	$fields_num = mysqli_num_fields($result);
+      	echo "<h1>Restaurants:</h1>";
+      	//echo "<table id='t01' border='1'><tr>";
+
+      	while($row = mysqli_fetch_row($result)) {
+      echo"<div class='restaurant'>
+          <div class='restaurant-contents'>
+            <div class='restaurant-image-container'>
+              <img src=$row[6]>
+            </div>
+            <div class='restaurant-info-container'>
+              <a href='#' class='restaurant-title'>$row[0]</a> <span class='restaurant-rating'>Rating: $row[7]</span> <span class='restaurant-city'>$row[3]</span>
+            </div>
           </div>
-          <div class='restaurant-info-container'>
-            <a href='#' class='restaurant-title'>$row[0]</a> <span class='restaurant-rating'>Rating: $row[7]</span> <span class='restaurant-city'>$row[3]</span>
-          </div>
-        </div>
-      </div>";
+        </div>";
+      }
     }
-
-    /* printing table headers
-    	for($i=0; $i<$fields_num; $i++) {
-    		$field = mysqli_fetch_field($result);
-    		echo "<td><b>$field->name</b></td>";
-    	}
-    	echo "</tr>\n";
-    	while($row = mysqli_fetch_row($result)) {
-    		echo "<tr>";
-    		// $row is array... foreach( .. ) puts every element
-    		// of $row to $cell variable
-    		foreach($row as $cell)
-    			echo "<td>$cell</td>";
-    		echo "</tr>\n";
-    	}*/
 
     	mysqli_free_result($result);
     	mysqli_close($conn);
 ?>
+<section>
+  <h2> <?php echo $msg; ?> </h2>
+
+<form method="post" id="addForm">
+<fieldset>
+<legend>Search:</legend>
+
+  <p>
+    <label for='name'>Name: </label>
+    <input type='text' name='name' id='name' class='optional'>
+  </p>
+
+  <p>
+    <label for='rating'>Overall Rating: </label>
+    <input type='number' min = 0 max = 5 name='rating' id='rating' class='optional'>
+  </p>
+
+  <p>
+    <label for='type'>Type: </label>
+    <select id='type' class='optional' name='type'>
+      <option selected value='*'>Any</option>
+      <option>Other</option>
+      <option>American</option>
+      <option>Barbecue</option>
+      <option>Casual Dining</option>
+      <option>Fast Food</option>
+      <option>Italian</option>
+      <option>Mexican</option>
+      <option>Northwest</option>
+      <option>Pizza</option>
+    </select>
+  </p>
+
+  <p>
+    <label for='city'>City: </label>
+    <input type='text' name='city' id='city' class='optional'>
+  </p>
+</fieldset>
+
+<p>
+  <input type = "submit"  value = "Submit" />
+  <input type = "reset"  value = "Clear Form" />
+</p>
+
+</form>
 
 <form method="get" action="addRestaurant.php">
   <button style="display: inline-block" type="submit">Add New Restaurant</button>
